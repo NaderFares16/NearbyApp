9f4f29a0598678a9851098471c4b27bf3b3cf3ea
Nader Fares <naderalifares16@hotmail.com>
2024-12-14 01:49:23 -0300

    feat: add expo-camera-view and configure QR lock with useEffect

--- src/app/market/[id].tsx
+++ src/app/market/[id].tsx
@@ -1,6 +1,7 @@
-import { useEffect, useState } from "react";
-import { View, Alert, Modal } from "react-native";
+import { useEffect, useState, useRef } from "react";
+import { View, Alert, Modal, StatusBar, ScrollView } from "react-native";
 import { router, useLocalSearchParams, Redirect } from "expo-router"
+import { useCameraPermissions, CameraView } from "expo-camera"
 
 import { Loading } from "@/components/loading";
 import { Cover } from "@/components/market/cover";
@@ -9,6 +10,7 @@ import { Coupon } from "@/components/market/coupon";
 import { Button } from "@/components/button";
 
 import { api } from "@/services/api";
+import { LightSpeedInLeft } from "react-native-reanimated";
 
 type DataProps = PropsDetails & {
   cover: string
@@ -20,8 +22,13 @@ export default function Market() {
   const [coupon, setCoupon] = useState<string | null>(null)
   const [isloading, setIsLoading] = useState(true)
   const [isVisibleCameraModal, setIsVisibleCameraModal] = useState(false)
+  const [couponIsFetching, setCouponIsFetching] = useState(false)
 
+  const [permission, requestPermission] = useCameraPermissions()
   const params = useLocalSearchParams<{ id: string }>()
+
+  const qrLock = useRef(false)
+  console.log(params.id)
 
   async function fetchMarket() {
     try {
@@ -36,8 +43,16 @@ export default function Market() {
     }
   }
 
-  function handleOpenCamera() {
+  async function handleOpenCamera() {
     try {
+      const {granted} = await requestPermission()
+
+      if (!granted) {
+        return Alert.alert("Camera Permission", "Camera access is currently restricted")
+      }
+
+      qrLock.current = false
+
       setIsVisibleCameraModal(true)
     } catch (error) {
       console.log(error)
@@ -45,9 +60,34 @@ export default function Market() {
     }
   }
 
+  async function getCoupon(id: string) {
+    try {
+      setCouponIsFetching(true)
+
+      const { data } = await api.patch("/coupons/" + id)
+
+      Alert.alert("Coupon", data.coupon)
+
+    } catch (error) {
+      console.log(error)
+      Alert.alert("Error", "Unable to use coupon")
+    } finally {
+      setCouponIsFetching(false)
+    }
+  }
+
+  function handleUseCoupon(id: string) {
+    setIsVisibleCameraModal(false)
+
+    Alert.alert("Coupon", "Coupon already redeemed", [
+      { style: "cancel", text: "Cancel" },
+      { text: "Continue", onPress: () => getCoupon(id) },
+    ])
+  }
+
   useEffect(() => {
     fetchMarket()
-  }, [params.id])
+  }, [params.id, coupon])
 
   if (isloading) {
     return <Loading />
@@ -59,9 +99,13 @@ export default function Market() {
 
   return (
     <View style={{ flex: 1 }}>
-      <Cover uri={data.cover} />
-      <Details data={data} />
-      {coupon && <Coupon code={coupon} />}
+      <StatusBar barStyle={"light-content"} hidden={isVisibleCameraModal} />
+
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <Cover uri={data.cover} />
+        <Details data={data} />
+        {coupon && <Coupon code={coupon} />}
+      </ScrollView>
 
       <View style={{ padding: 32 }}>
         <Button onPress={handleOpenCamera}>
@@ -72,8 +116,18 @@ export default function Market() {
       </View>
 
       <Modal style={{ flex: 1 }} visible={isVisibleCameraModal}>
-        <View style={{ flex: 1, justifyContent: "center" }}>
-          <Button onPress={() => setIsVisibleCameraModal(false)}>
+        <CameraView style={{ flex: 1 }}
+          facing="back"
+          onBarcodeScanned={({ data }) => {
+            if (data && !qrLock.current) {
+              qrLock.current = true
+              setTimeout(() => handleUseCoupon(data), 500)
+            }
+          }}
+        />
+
+        <View style={{ position: "absolute", bottom: 32, left: 32, right: 32 }}>
+          <Button onPress={() => setIsVisibleCameraModal(false)} isLoading={couponIsFetching}>
             <Button.Title>Back</Button.Title>
           </Button>
         </View>
